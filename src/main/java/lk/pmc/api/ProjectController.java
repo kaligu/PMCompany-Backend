99a558964ba9fb975e77f6a9c2f7b040cf3c80df
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-09-21 13:47:15 +0530

    fix bugs

--- src/main/java/lk/pmc/api/ProjectController.java
+++ src/main/java/lk/pmc/api/ProjectController.java
@@ -6,10 +6,19 @@
 */
 package lk.pmc.api;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author : H.C.Kaligu Jayanath
  * Date    : 9/20/2023
  * Time    : 11:47 PM
  */
+
+@RestController
+@RequestMapping("/project")
+@CrossOrigin
 public class ProjectController {
+
 }
--- src/main/java/lk/pmc/api/TechLeadController.java
+++ src/main/java/lk/pmc/api/TechLeadController.java
@@ -6,10 +6,29 @@
 */
 package lk.pmc.api;
 
+import lk.pmc.dto.TechLeadDTO;
+import lk.pmc.service.custom.TechLeadService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
 /**
  * @author : H.C.Kaligu Jayanath
  * Date    : 9/20/2023
  * Time    : 11:47 PM
  */
+
+@RestController
+@RequestMapping("/techlead")
+@CrossOrigin
 public class TechLeadController {
+
+    @Autowired
+    TechLeadService techLeadService;
+
+    @PostMapping(path = "/save", consumes = MediaType.APPLICATION_JSON_VALUE)
+    boolean saveTechLead(@RequestBody TechLeadDTO techLeadDTO) {
+        System.out.println("Requested a new TechLead: " + techLeadDTO.getTechLeadName());
+        return techLeadService.saveTechLead(techLeadDTO);
+    }
 }
